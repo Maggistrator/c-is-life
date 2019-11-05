bb6b2fd70fa9463189dc2d1fd4e93f2d5ba6d8d8
Maggistrator <lira_hertstrings@mail.ru>
2019-11-06 01:17:23 +0300

    hotfix on 4th

--- LabII/task4.c
+++ LabII/task4.c
@@ -7,10 +7,7 @@
 * Задача оформлена процедурой, ибо тут возвращать результат не удобно
 */
 void EORow(int N){
-    if(N > 2){
-        if(N % 2) for(int i = 1; i <= N; i+=2) printf("%d  ", i);
-        else for(int i = 2; i <= N; i+=2) printf("%d  ", i);
-    }
-    else printf("Значение не удовлетворяет условию задачи.");
+    if(N % 2) for(int i = 1; i <= N; i+=2) printf("%d  ", i);
+    else for(int i = 2; i <= N; i+=2) printf("%d  ", i);
     printf("\n");
 }
