8d16ff4ae955731eb258471e29cacb1e4de698aa
Maggistrator <lira_hertstrings@mail.ru>
2020-01-24 09:47:36 -0800

    finished task 5 in lab 4

--- LabIV/booksym.c
+++ LabIV/booksym.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define PAGES 450
-#define LINES_PER_PAGE 80
+#include <string.h>
+
+#define PAGES 2
+#define LINES_PER_PAGE 2
 #define SYMBOLS_PER_LINE 64
 
 
@@ -18,17 +20,34 @@ int main(){
         printf("Страница %d. \
         \nЗаполните её построчно.\
         \nВам доступно %d строк, и %d символов на строку.\n",
-        i, LINES_PER_PAGE, SYMBOLS_PER_LINE);
+        i+1, LINES_PER_PAGE, SYMBOLS_PER_LINE);
 
-        char **page = *(book+i);
-        *page = (char**)malloc(sizeof(char*) * LINES_PER_PAGE);
+        *(book+i) = (char**)malloc(sizeof(char*) * LINES_PER_PAGE);
         for(int j = 0; j < LINES_PER_PAGE; j++){
-            printf("%d:",j);
-            char *line = *(page+j);
-            fgets(line, SYMBOLS_PER_LINE, stdin);
-            printf("you've got: \"%s\" here", line);
+            printf("%d:",j+1);
+            *(*(book+i)+j) = (char*)malloc(sizeof(char)*SYMBOLS_PER_LINE);
+            fgets(*(*(book+i)+j), SYMBOLS_PER_LINE, stdin);
+            strtok(*(*(book+i)+j), "\n");
         }
     }
+
+    int x = 0, y = 0, isCorrect = 0;
+    printf("Введите номер страницы и номер строки, которую нужно напечатать.\n");
+    while(!isCorrect){
+        puts("Страница:");
+        scanf("%d", &x);
+        puts("Строка:");
+        scanf("%d", &y);
+        if(x > 0 && x <= PAGES){
+            if(y > 0 && y <= LINES_PER_PAGE){
+                isCorrect = 1;
+            } else puts("Нет такой строки, попробуйте ещё раз.");
+        } else puts("Нет такой страницы, попробуйте ещё раз.");
+    }
+
+    char **page = *(book+x-1);
+    char *line = *(page+y-1);
+    printf("Это строка: %s", line);
 
     for(int i = 0; i < PAGES; i++){
         char **page = *(book+i);
