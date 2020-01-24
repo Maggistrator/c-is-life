fb169e0a53ef751dc22ce9efd4b1688da060381d
Maggistrator <lira_hertstrings@mail.ru>
2020-01-24 10:05:28 +0300

    win is not for c-development

--- LabIV/symmatrix.c
+++ LabIV/symmatrix.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "arrayutils.h"
+#include <float.h>
 
 #define LEN 4
 
 int main4(){
-    double **matrix = (double**)malloc(sizeof(char*) * LEN);
-    for(int i = 0; i < LEN; i++) dfillarran(*(matrix + i), LEN);
+    double **matrix = (double**)malloc(sizeof(double*) * LEN);
 
-    for(int i = 0; i < LEN; i++){
-        for(int j = LEN; j > 0; j--){
-
+    printf("Введите массив из %d чисел, через пробел: ", LEN);
+    for(int i = 0; i < LEN; i++) {
+        for(int j = 0; j < LEN; j++) {
+            printf("Введите %d-й элемент матрицы:9",i+j);
+            char* source = fgets(source, 32, stdin);
+            *(*(matrix+i)+j)= atoi(source);
         }
+    }
+    printmx(matrix, LEN, LEN);
+
+    for(int i = 0;  i < LEN; i++){
+        for(int j = 0; j < LEN; j++){
+            double element = *(*(matrix + i)+j);
+            double symmetric_element = *(*(matrix + LEN-j)+LEN-i);
+            if(element - symmetric_element > DBL_EPSILON) {
+                printf("Матрица НЕ симметрична относительно побочной диагонали.");
+                break;
+            }
+        }
+        if(!(LEN-i)) printf("Матрица симметрична относительно побочной диагонали!");
     }
 
     for(int i = 0; i < LEN; i++) free(*(matrix + i));
